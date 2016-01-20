63890ddbf5e211e022734fff56cac7b70305e6de
Victoria Hooker <vhooker@cnm.edu>
2016-01-20 11:50:57 -0700

    added some html

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,6 @@
 		<title>the Hackey Hooker</title>
 	</head>
 	<body>
-		
+		<p>Hi, I'm Corrie Hooker and I build websites that are easy to use. I can help offer you and your clients a great online experience</p>
 	</body>
 </html>
