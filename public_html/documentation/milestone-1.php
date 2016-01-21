60e1684092950d07ca391407d0a1d7ce6872f4f2
Victoria Hooker <vhooker@cnm.edu>
2016-01-21 16:29:12 -0700

    Added some content for the persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -2,9 +2,44 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
-		<title>the Hackey Hooker</title>
+		<link rel="stylesheet" href="../css/stylesheet.css">
+		<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+		<title>The Hackey Hooker</title>
 	</head>
 	<body>
-		<p>Hi, I'm Corrie Hooker and I build websites that are easy to use. I can help offer you and your clients a great online experience</p>
+		<div class="body">
+			<h1>Meet Creative Corrie</h1>
+			<h1>as the Hackey Hooker</h1>
+			<p>PURPOSE: Hi, I'm Corrie Hooker and I build websites that are easy to use. I can help offer you and your clients a great online experience.</p>
+			<p>AUDIENCE:</p>
+			<p>Clients who are looking for a professional and well rounded coder who is also easy to work with</p>
+			<p>WEBSITE GOAL:</p>
+			<p>The goal of this website is to showcase my talent, skills and work.</p>
+
+			Name: John Miller
+			Age
+			Gender
+			tasks
+			motivation
+
+			Design targets
+			Stereo-types
+
+
+<!---->
+<!--			<div class="boxtitle">Meet Creative Corrie</div>-->
+<!--			<div class="box">-->
+<!--				<span class="boxheading">Multi-Faceted</span><br>-->
+<!--				Name: Victoria Senolee Hooker<br>-->
+<!--				Born: Route 66, Zuzax Exit.. It's now a Park and Ride. Ionic, isn't it?<br>-->
+<!--				Career: Creator<br>-->
+<!--				Education: Self-taught BS in Life, going for my Masters. If that doesn't kill me, I'll start thinking about my PHD.<br>-->
+<!--				Ethnicity: I'm so mixed, I call myself American.<br>-->
+<!--				Life's Lesson: If you are depressed, you are living in the past. If you are anxious, you are living in the future. Neither is in your control. Live in the moment and do the very best you can. In it's own way, it does soften the past and makes for a stronger tomorrow. <br>-->
+<!---->
+<!--				<span class="boxheading">Course Goals</span><br>-->
+<!--				Right now, my greatest goal is to get this flippin' page working if it kills me.<br>-->
+<!--			</div><br>-->
+
 	</body>
 </html>
