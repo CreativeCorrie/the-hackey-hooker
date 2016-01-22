eb3e0bb0286074ae3b00c56870817c5ac72782a8
Victoria Hooker <vhooker@cnm.edu>
2016-01-22 08:56:31 -0700

    made it look nicer with css

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,40 +7,28 @@
 		<title>The Hackey Hooker</title>
 	</head>
 	<body>
-		<div class="body">
+
 			<h1>Meet Creative Corrie</h1>
 			<h1>as the Hackey Hooker</h1>
+		<div class="boxtitle">Define:</div>
+		<div class="box">
 			<p>PURPOSE: Hi, I'm Corrie Hooker and I build websites that are easy to use. I can help offer you and your clients a great online experience.</p>
 			<p>AUDIENCE:</p>
 			<p>Clients who are looking for a professional and well rounded coder who is also easy to work with</p>
 			<p>WEBSITE GOAL:</p>
-			<p>The goal of this website is to showcase my talent, skills and work.</p>
-			<div>
-			Name: Jon T. Williams
-			Age:46
+			<p>The goal of this website is to showcase my talent, skills and work.</div>
+
+			<div class="boxtitle">Persona</div>
+			<div class="box">
+			Name: Jon T. Williams<br>
+			Age:46<br>
 			<!--The collective summary of their learnings is: the average entrepreneur is 40 when they launch their startup. People over 55 are twice as likely as people under 35 to launch a high-growth startup. The average age of a successful startup with over $1 million in revenues was 39. Age was less of a driver to entrepreneurial success than previous startup and industry experience. http://www.entrepreneur.com/article/235357-->
-			Gender: Male
-			Tasks: Build an app that morphs a person selfie into what they would look like with various ailments that are preventable with vaccinations
-			Motivation: A professional statistician in the healthcare field who created a startup for a health app that would help that industry with vaccinations. He has no experience in the technology and programming needed and wants to hire freelance coders and build a team with a wider scope of expertise.
-			Use Case:
-			Jon is not the guru when it comes to technology, however he is a savvy entrepreneur. He has a great idea that he believes will help humanity and make money at the same time. Having the mindset that one cannot make money without spending it, he knows he can hire the talent he needs to hire a team to build his dream app. He surfs the web for people who have nice presentations and are knowledgeable with good reputations. He comes to The Hackey Hooker and is amazed by what finds there.
+			Gender: Male<br>
+			Tasks: Build an app that morphs a person selfie into what they would look like with various ailments that are preventable with vaccinations<br>
+			Motivation: A professional statistician in the healthcare field who created a startup for a health app that would help that industry with vaccinations. He has no experience in the technology and programming needed and wants to hire freelance coders and build a team with a wider scope of expertise.</div>
+			<div class="boxtitle">Use Case:</div>
+			<div class="box">
+			Jon is not the guru when it comes to technology, however he is a savvy entrepreneur. He has a great idea that he believes will help humanity and make money at the same time. Having the mindset that one cannot make money without spending it, he knows he can hire the talent he needs to hire a team to build his dream app. He surfs the web for people who have nice presentations and are knowledgeable with good reputations. He comes to The Hackey Hooker and is amazed by what finds there.</div>
 			</div>
-
-
-<!---->
-<!--			<div class="boxtitle">Meet Creative Corrie</div>-->
-<!--			<div class="box">-->
-<!--				<span class="boxheading">Multi-Faceted</span><br>-->
-<!--				Name: Victoria Senolee Hooker<br>-->
-<!--				Born: Route 66, Zuzax Exit.. It's now a Park and Ride. Ionic, isn't it?<br>-->
-<!--				Career: Creator<br>-->
-<!--				Education: Self-taught BS in Life, going for my Masters. If that doesn't kill me, I'll start thinking about my PHD.<br>-->
-<!--				Ethnicity: I'm so mixed, I call myself American.<br>-->
-<!--				Life's Lesson: If you are depressed, you are living in the past. If you are anxious, you are living in the future. Neither is in your control. Live in the moment and do the very best you can. In it's own way, it does soften the past and makes for a stronger tomorrow. <br>-->
-<!---->
-<!--				<span class="boxheading">Course Goals</span><br>-->
-<!--				Right now, my greatest goal is to get this flippin' page working if it kills me.<br>-->
-<!--			</div><br>-->
-
 	</body>
 </html>
---
+++ public_html/documentation/parking-lot.php
@@ -0,0 +1,15 @@
+
+<!---->
+<!--			<div class="boxtitle">Meet Creative Corrie</div>-->
+<!--			<div class="box">-->
+<!--				<span class="boxheading">Multi-Faceted</span><br>-->
+<!--				Name: Victoria Senolee Hooker<br>-->
+<!--				Born: Route 66, Zuzax Exit.. It's now a Park and Ride. Ionic, isn't it?<br>-->
+<!--				Career: Creator<br>-->
+<!--				Education: Self-taught BS in Life, going for my Masters. If that doesn't kill me, I'll start thinking about my PHD.<br>-->
+<!--				Ethnicity: I'm so mixed, I call myself American.<br>-->
+<!--				Life's Lesson: If you are depressed, you are living in the past. If you are anxious, you are living in the future. Neither is in your control. Live in the moment and do the very best you can. In it's own way, it does soften the past and makes for a stronger tomorrow. <br>-->
+<!---->
+<!--				<span class="boxheading">Course Goals</span><br>-->
+<!--				Right now, my greatest goal is to get this flippin' page working if it kills me.<br>-->
+<!--			</div><br>-->
