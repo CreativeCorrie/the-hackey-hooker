e5843af7bd9b70ff405bc3c250222dc5e1774cb7
Victoria Hooker <vhooker@cnm.edu>
2016-01-22 08:27:14 -0700

    Added Use Case

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -15,15 +15,16 @@
 			<p>Clients who are looking for a professional and well rounded coder who is also easy to work with</p>
 			<p>WEBSITE GOAL:</p>
 			<p>The goal of this website is to showcase my talent, skills and work.</p>
-
-			Name: John Miller
-			Age
-			Gender
-			tasks
-			motivation
-
-			Design targets
-			Stereo-types
+			<div>
+			Name: Jon T. Williams
+			Age:46
+			<!--The collective summary of their learnings is: the average entrepreneur is 40 when they launch their startup. People over 55 are twice as likely as people under 35 to launch a high-growth startup. The average age of a successful startup with over $1 million in revenues was 39. Age was less of a driver to entrepreneurial success than previous startup and industry experience. http://www.entrepreneur.com/article/235357-->
+			Gender: Male
+			Tasks: Build an app that morphs a person selfie into what they would look like with various ailments that are preventable with vaccinations
+			Motivation: A professional statistician in the healthcare field who created a startup for a health app that would help that industry with vaccinations. He has no experience in the technology and programming needed and wants to hire freelance coders and build a team with a wider scope of expertise.
+			Use Case:
+			Jon is not the guru when it comes to technology, however he is a savvy entrepreneur. He has a great idea that he believes will help humanity and make money at the same time. Having the mindset that one cannot make money without spending it, he knows he can hire the talent he needs to hire a team to build his dream app. He surfs the web for people who have nice presentations and are knowledgeable with good reputations. He comes to The Hackey Hooker and is amazed by what finds there.
+			</div>
 
 
 <!---->
