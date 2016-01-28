9b2024690e246c2155c2df87b8e174a0ff6158b7
Victoria Hooker <vhooker@cnm.edu>
2016-01-28 09:32:48 -0700

    Added wireframe jpgs for 3 pages in layout

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -1,1 +1,3 @@
-<img src="../images"
+<img src="../images/wireframe0.jpg" rel="Landing Page">
+<img src="../images/wireframe1.jpg" rel="Contact page">
+<img src="../images/wireframe2.jpg" rel="Another Layout">
