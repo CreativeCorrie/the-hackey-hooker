3dfa54ff857b3fe2db7887b92fe93176319fbce6
Victoria Hooker <vhooker@cnm.edu>
2016-02-28 14:02:07 -0700

    Made work page to merge from another project

---
+++ public_html/documentation/work.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<!-- The 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta charset="utf-8"/>
+		<meta http-equiv="X-UA-COMPATIBLE" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+		<!-- Optional theme -->
+		<link rel="stylesheet" href=//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- My Custom CSS rlewis37@cnm.edu -->
+		<link rel="stylesheet" href="../css/style.css" type="text/css"/>
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (required for Bootstap's JS plugins) -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<title>My Work</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+
+			<!--this is where the header and navbar go-->
+			<header>
+				<div class="container-fluid">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="home.html">
+							<img alt="Logo" src="../images/logo2.png" height="40" width="100">
+						</a>
+					</div>
+
+
+					<div class="btn-group" role="group" aria-label="...">
+						<a class="navbar-brand">
+							<button type="button" class="btn btn-default">Home</button>
+							<link href="home.html">
+							<button type="button" class="btn btn-default">My Work</button>
+							<link href="work.html">
+							<button type="button" class="btn btn-default">Education</button>
+							<link href="education.html">
+							<button type="button" class="btn btn-default">Background</button>
+							<link href="background.html">
+							<button type="button" class="btn btn-default">Contact Me</button>
+							<link href="contact.html">
+						</a>
+
+					</div>
+
+
+				</div><!-- /.navbar-collapse -->
+
+			</header>
+
+			<!--this is where the main content goes-->
+			<main>
+				<div class="container">
+
+					<!--begin welcome row-->
+					<div class="row">
+						<div class="col-md-12">
+						</div>
+					</div>
+
+					<!--begin welcome row-->
+					<div class="row">
+						<div class="col-md-12">
+							<div class="jumbotron">
+								<h1>My Work & Resume
+									<img alt="Logo" src="../images/logo.png" height="80" width="200">
+								</h1>
+								<p>A collection of my work & experience!</p>
+							</div>
+
+							<div class="container-fluid">
+								<div class="row">
+
+									<div class="content-box">
+										<div class="row">
+											<h2>Core Competencies</h2>
+										</div>
+									</div>
+
+									<div class="content-box">
+										<div class="row">
+											<h2>Education</h2>
+
+											<!-- Deep Dive Header-->
+											<div class="content-box-nest">
+												<div class="row">
+													<div class="col-md-8">
+														<h3>CNM STEMulus Center Deep Dive Coders Bootcamp</h3>
+													</div>
+													<div class="col-md-4">
+														<h3>Albuquerque, NM</h3>
+													</div>
+												</div>
+											</div>
+
+											<!--Deep Dive Contents-->
+											<div class="content-box-nest-1">
+												<div class="row">
+													<h4>Capstone Project:</h4>
+													<div class="col-md-12">
+														<p>Project: Co-developer of Web-based, customized enterprise software solution that integrated business process for scheduling employees.</p>
+													</div>
+												</div>
+
+												<div class="content-box-nest-1">
+													<div class="row">
+														<h4>Coursework to include:</h4>
+														<div class="col-md-4">PHP 7</div>
+														<div class="col-md-4">MySQL</div>
+														<div class="col-md-4">JavaScript</div>
+														<div class="col-md-4">Enterprise Application Development</div>
+														<div class="col-md-4">Security</div>
+														<div class="col-md-4">It Project Management</div>
+														<div class="col-md-4">Web Design</div>
+														<div class="col-md-4">Compute Architecture</div>
+														<div class="col-md-4">Object Oriented Programming</div>
+														<div class="col-md-4">Bootstrap</div>
+														<div class="col-md-4">CSS 3</div>
+													</div>
+												</div>
+											</div>
+
+											<!--Girl Develop It! Header-->
+											<div class="content-box-nest">
+												<div class="row">
+													<div class="col-md-8">
+														<h3>Girl Develop It!</h3>
+													</div>
+													<div class="col-md-4">
+														<h3>Albuquerque, NM</h3>
+													</div>
+												</div>
+											</div>
+
+											<!--Girl Develop It! Contents-->
+											<div class="content-box-nest-1">
+												<div class="row">
+													<h4>Coursework to include:</h4>
+													<div class="col-md-4">HTML5</div>
+													<div class="col-md-4">CSS 3</div>
+													<div class="col-md-4">WAMP</div>
+													<div class="col-md-4">PHP</div>
+													<div class="col-md-4">MySQL</div>
+												</div>
+											</div>
+
+											<!--Business of Art Header-->
+											<div class="content-box-nest">
+												<div class="row">
+													<div class="col-md-8">
+														<h3>Anderson School of Business, The Art of Business</h3>
+													</div>
+													<div class="col-md-4">
+														<h3>Albuquerque, NM</h3>
+													</div>
+												</div>
+											</div>
+
+											<!--Business of Art Contents-->
+											<div class="content-box-nest-1">
+												<div class="row">
+													<h4>Coursework to include:</h4>
+													<div class="col-md-12">Branding and Marketing Strategy</div>
+												</div>
+											</div>
+
+											<!--Team Works Header-->
+											<div class="content-box-nest">
+												<div class="row">
+													<div class="col-md-8">
+														<h3>NM Commission on the Status of Women, Team Works</h3>
+													</div>
+													<div class="col-md-4">
+														<h3>Albuquerque, NM</h3>
+													</div>
+												</div>
+											</div>
+
+											<!--Team Words Contents-->
+											<div class="content-box-nest-1">
+												<div class="row">
+													<h4>Coursework to include:</h4>
+													<div class="col-md-12">Computer science, user end information technology and interpersonal skills.</div>
+												</div>
+											</div>
+
+											<!--Sprint PCS Header-->
+											<div class="content-box-nest">
+												<div class="row">
+													<div class="col-md-8">
+														<h3>Sprint PCS</h3>
+													</div>
+													<div class="col-md-4">
+														<h3>Albuquerque, NM</h3>
+													</div>
+												</div>
+											</div>
+
+											<!--Sprint PCS Contents-->
+											<div class="content-box-nest-1">
+												<div class="row">
+													<h4>Coursework to include:</h4>
+													<div class="col-md-12">technical and account services</div>
+												</div>
+											</div>
+
+											<!--Townsend Training Header-->
+											<div class="content-box-nest">
+												<div class="row">
+													<div class="col-md-8">
+														<h3>Townsend Training</h3>
+													</div>
+													<div class="col-md-4">
+														<h3>Albuquerque, NM</h3>
+													</div>
+												</div>
+											</div>
+
+											<!--Townsend Training Contents-->
+											<div class="content-box-nest-1">
+												<div class="row">
+													<h4>Coursework to include:</h4>
+													<div class="col-md-4">Telecommunications</div>
+													<div class="col-md-4">Time and Money Management</div>
+													<div class="col-md-4">Problem Solving and Interpersonal Skills</div>
+												</div>
+											</div>
+
+											<!--Project Phoenix Header-->
+											<div class="content-box-nest">
+												<div class="row">
+													<div class="col-md-8">
+														<h3>Project Phoenix</h3>
+													</div>
+													<div class="col-md-4">
+														<h3>Albuquerque, NM</h3>
+													</div>
+												</div>
+											</div>
+
+											<!--Project Phoenix Contents-->
+											<div class="content-box-nest-1">
+												<div class="row">
+													<h4>Coursework to include:</h4>
+													<div class="col-md-4">Computer Literacy</div>
+													<div class="col-md-4">Team Building</div>
+													<div class="col-md-4">Change Management</div>
+												</div>
+											</div>
+
+											<!--CNM Header-->
+											<div class="content-box-nest">
+												<div class="row">
+													<div class="col-md-8">
+														<h3>Central New Mexico Community College</h3>
+													</div>
+													<div class="col-md-4">
+														<h3>Albuquerque, NM</h3>
+													</div>
+												</div>
+											</div>
+
+											<!--CNM Contents-->
+											<div class="content-box-nest-1">
+												<div class="row">
+													<h4>Coursework to include:</h4>
+													<div class="col-md-6">Computer Science</div>
+													<div class="col-md-6">Information Technology</div>
+												</div>
+											</div>
+
+											<!--CNM Header-->
+											<div class="content-box-nest">
+												<div class="row">
+													<div class="col-md-8">
+														<h3>University of New Mexico</h3>
+													</div>
+													<div class="col-md-4">
+														<h3>Albuquerque, NM</h3>
+													</div>
+												</div>
+											</div>
+
+											<!--CNM Contents-->
+											<div class="content-box-nest-1">
+												<div class="row">
+													<h4>Coursework to include:</h4>
+													<div class="col-md-12">High Ropes Course Completion</div>
+												</div>
+											</div>
+
+											<!--Wickenburg Header-->
+											<div class="content-box-nest">
+												<div class="row">
+													<div class="col-md-8">
+														<h3>Wickenburg High School</h3>
+													</div>
+													<div class="col-md-4">
+														<h3>Wickenburg, Az.</h3>
+													</div>
+												</div>
+											</div>
+
+											<!--Wickenburg Contents-->
+											<div class="content-box-nest-1">
+												<div class="row">
+													<h4>Coursework to include:</h4>
+													<div class="col-md-12">Diploma, focus on English and History	</div>
+												</div>
+											</div>
+										</div>
+									</div>
+
+									<div class="content-box">
+										<div class="row">
+											<h2>Technology</h2>
+										</div>
+									</div>
+
+									<div class="content-box">
+										<div class="row">
+											<h2>Work Experience</h2>
+										</div>
+									</div>
+
+									<div class="content-box">
+										<div class="row">
+											<h2>Volunteer Experience & Causes</h2>
+										</div>
+									</div>
+
+									<div class="content-box">
+										<div class="row">
+											<h2>Memberships & Associations</h2>
+										</div>
+									</div>
+
+									<div class="content-box">
+										<div class="row">
+											<h2>Personal Profile</h2>
+										</div>
+									</div>
+
+									<div class="content-box">
+										<div class="row">
+											<h2>Achievements and Awards</h2>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+
+			<!--begin footer content-->
+			<footer>
+				<div class="container">
+					&copy; Creative Corrie Artista, LLC
+				</div>
+			</footer>
+
+		</div> <!-- /.sfooter collapse-->
+
+	</body>
+</html>
