bcd3c92b22dead935d49cee601eaf23505089fdc
Victoria Hooker <vhooker@cnm.edu>
2016-03-06 13:46:18 -0700

    Got all the headings done Got all the nav bar links done Started beefing up the education area.

--- public_html/documentation/background.php
+++ public_html/documentation/background.php
@@ -58,25 +58,23 @@
 							<span class="icon-bar"></span>
 						</button>
 						<a class="navbar-brand" href="home.php">
-							<img alt="Logo" src="../images/logo2.png" height="40" width="100">
+							<img alt="Logo" src="../images/logo2.png" height="20" width="50">
 						</a>
 					</div>
 
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="nav navbar-nav">
-							<li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-							<li><a href="#">Link</a></li>
 							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
 								<ul class="dropdown-menu">
-									<li><a href="#">Action</a></li>
-									<li><a href="#">Another action</a></li>
-									<li><a href="#">Something else here</a></li>
+									<li><a href="#">Hello</a></li>
+									<li><a href="#Education">Education</a></li>
+									<li><a href="#Work">Work Experience</a></li>
 									<li role="separator" class="divider"></li>
-									<li><a href="#">Separated link</a></li>
+									<li><a href="#Samples">My Samples</a></li>
 									<li role="separator" class="divider"></li>
-									<li><a href="#">One more separated link</a></li>
+									<li><a href="#">Contact Me Now</a></li>
 								</ul>
 							</li>
 						</ul>
@@ -87,15 +85,18 @@
 							<button type="submit" class="btn btn-default">Submit</button>
 						</form>
 						<ul class="nav navbar-nav navbar-right">
-							<li><a href="#">Link</a></li>
+							<li><a href="#">My Art Site</a></li>
 							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Menu <span class="caret"></span></a>
 								<ul class="dropdown-menu">
-									<li><a href="#">Action</a></li>
-									<li><a href="#">Another action</a></li>
-									<li><a href="#">Something else here</a></li>
+									<li><a href="http://creativecorrie.com/">Home</a></li>
+									<li><a href="http://creativecorrie.com/about/">About</a></li>
+									<li><a href="http://creativecorrie.com/my-work/">My Work</a></li>
 									<li role="separator" class="divider"></li>
-									<li><a href="#">Separated link</a></li>
+									<li><a href="http://creativecorrie.com/my-services/">My Services</a></li>
+									<li role="separator" class="divider"></li>
+									<li><a href="http://creativecorrie.com/contact/">Contact Me</a></li>
+
 								</ul>
 							</li>
 						</ul>
@@ -122,9 +123,9 @@
 					<img class="first-slide" src="../images/cca-cover.jpg" alt="First slide">
 					<div class="container">
 						<div class="carousel-caption">
-							<h1>Creative marketing visionary.</h1>
-							<p>I'm skilled in graphic and web design, databases, social media and building strong business alliances in the community.</p>
-							<p><a class="btn btn-lg btn-primary" href="#" role="button">Contact me now</a></p>
+							<h1>Creative marketing visionary, web developer and artist.</h1>
+							<p>I'm skilled in graphic and web design, databases, MySQL, PHP, social media and building strong business alliances in the community.</p>
+							<p><a class="btn btn-lg btn-primary" href="http://creativecorrie.com/contact/" role="button">Contact me now</a></p>
 						</div>
 					</div>
 				</div>
@@ -174,23 +175,28 @@
 			<div class="row">
 				<div class="col-lg-4">
 					<img class="img-circle" src="../images/sun-daisy.jpg" alt="Generic placeholder image" width="140" height="140">
-					<h2>Educaiton</h2>
-					<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+					<h2>Education</h2>
+					<p>Technology moves pretty fast, if you don't stop and educate yourself once in a while, you could miss it.</p>
+					<p><em>~Tweaked from Ferris Bueller</em></p>
 					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 				</div><!-- /.col-lg-4 -->
 				<div class="col-lg-4">
 					<img class="img-circle" src="../images/stumped-mushrooms.jpg" alt="Generic placeholder image" width="140" height="140">
 					<h2>Experience</h2>
-					<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+					<p>You are the sum total of everything you've ever seen, been told, forgot; it's all there. Everthing influences each of us, and because of the I try to ake sure that my experiences are positive.</p>
+					<p><em>~Maya Angelou</em></p>
 					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 				</div><!-- /.col-lg-4 -->
 				<div class="col-lg-4">
 					<img class="img-circle" src="../images/dandilion.jpg" alt="Generic placeholder image" width="140" height="140">
 					<h2>Samples of My Work</h2>
-					<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+					<p>Being busy does not always mean real work. The object of all work is production or accomplishment and to either of these ends there must be forethought, system, planning, intelligence, and honest purpose, as well as perspiration. Seeming to do is not doing.</p>
+					<p><em>~Thomas A. Edison</em></p>
 					<p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
 				</div><!-- /.col-lg-4 -->
 			</div><!-- /.row -->
+			<p class="pull-right"><a href="#">Back to top</a></p>
+
 
 
 			<!-- START THE FEATURETTES -->
@@ -199,37 +205,96 @@
 
 			<div class="row featurette">
 				<div class="col-md-7">
-					<h2 class="featurette-heading">Educational highlights. <span class="text-muted">It'll blow your mind.</span></h2>
-					<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+					<h2 class="featurette-heading"><a name="Education">Educational highlights.</a>
+						<span class="text-muted">It'll blow your mind.</span>
+					</h2>
+
+					<div class="lead">
+						<ul class="list-unstyled">
+							<li>CNM STEMulus Center Deep Dive Coders Bootcamp</li>
+							<li>Girl Develop It!</li>
+							<li>NM Commission on the Status of Women, Team Works</li>
+							<li>Project Phoenix</li>
+							<li>Central New Mexico Community College</li>
+						</ul>
+					</div>
 				</div>
 				<div class="col-md-5">
 					<img class="featurette-image img-responsive center-block" src="../images/sun-daisy.jpg" alt="Generic placeholder image">
 				</div>
 			</div>
 
+
+			<div class="row">
+				<div class="col-sm-6 col-md-4">
+					<div class="thumbnail">
+						<img src="../images/icon-ddcb-lg.png" alt="Deep Dive Logo">
+						<div class="caption">
+							<h3>CNM STEMulus Center Deep Dive Coders Bootcamp</h3>
+							<h3>Albuquerque, NM</h3>
+							<h4>Capstone Project:</h4>
+							<p>Project: Co-developer of Web-based, customized enterprise software solution that integrated business process for scheduling employees.</p>
+							<h4>Coursework to include:</h4>
+								<ul>
+									<li>PHP 7</li>
+									<li>MySQL</li>
+									<li>JavaScript</li>
+									<li>Enterprise Application Development</li>
+									<li>Security</li>
+									<li>It Project Management</li>
+									<li>Web Design</li>
+									<li>Computer Architecture</li>
+									<li>Object Oriented Programming</li>
+									<li>Bootstrap 3</li>
+									<li>CSS 3</li>
+								</ul>
+
+							</div>
+
+							<p><a href="#" class="btn btn-primary" role="button">Capstone Project</a> <a href="http://stemuluscenter.org/deep-dive-coding-bootcamp-application/" class="btn btn-default" role="button">Apply For Deep Dive</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+
+
+			<p class="pull-right"><a href="#">Back to top</a></p>
+
+
 			<hr class="featurette-divider">
 
 			<div class="row featurette">
 				<div class="col-md-7 col-md-push-5">
-					<h2 class="featurette-heading">Work experience. <span class="text-muted">Been ever so busy.</span></h2>
+					<h2 class="featurette-heading"><a name="Work">Work experience.</a>
+						<span class="text-muted">Been ever so busy.</span>
+					</h2>
 					<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
 				</div>
 				<div class="col-md-5 col-md-pull-7">
 					<img class="featurette-image img-responsive center-block" src="../images/stumped-mushrooms.jpg" alt="Generic placeholder image">
 				</div>
 			</div>
+			<p class="pull-right"><a href="#">Back to top</a></p>
+
 
 			<hr class="featurette-divider">
 
 			<div class="row featurette">
 				<div class="col-md-7">
-					<h2 class="featurette-heading">And lastly, great examples of my work. <span class="text-muted">See for yourself.</span></h2>
-					<p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+					<h2 class="featurette-heading"><a name="Samples">And lastly, great examples of my work.</a>
+						<span class="text-muted">See for yourself.</span>
+					</h2>
+					<h3 class="lead">
+					<a href="http://creativecorrie.com/">CreativeCorie.com</a>
+					</h3>
 				</div>
 				<div class="col-md-5">
 					<img class="featurette-image img-responsive center-block" src="../images/dandilion.jpg" alt="Generic placeholder image">
 				</div>
 			</div>
+
 
 			<hr class="featurette-divider">
 
